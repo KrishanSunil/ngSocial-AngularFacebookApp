ec40fb785994670b05d96e1372b9d13617bd26b6
KrishanSunil <KrishanthaS@FICSG21468.ffe.foxeg.com>
2015-11-03 17:47:09 +0800

    Added Facebook appID
    Facebook Integration started

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -1,4 +1,4 @@
-var app = angular.module('ngSocial',['ngRoute'])
+var app = angular.module('ngSocial',['ngRoute','ngFacebook'])
 
 .config(['$routeProvider',function($routeProvider){
 	$routeProvider.
@@ -12,6 +12,33 @@ var app = angular.module('ngSocial',['ngRoute'])
 		});
 }])
 
-.controller('FacebookCtrl',[function(){
-	console.log('Inside FacebookCtrl');
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('1498614777106084');
+  $facebookProvider.setPermission("public_profile,email,user_birthday");
+})
+
+.run( function( $rootScope ) {
+  // Load the facebook SDK asynchronously
+  (function(){
+     // If we've already installed the SDK, we're done
+     if (document.getElementById('facebook-jssdk')) {return;}
+
+     // Get the first script element, which we'll use to find the parent node
+     var firstScriptElement = document.getElementsByTagName('script')[0];
+
+     // Create a new script element and set its id
+     var facebookJS = document.createElement('script'); 
+     facebookJS.id = 'facebook-jssdk';
+
+     // Set the new script's source to the source of the Facebook JS SDK
+     facebookJS.src = '//connect.facebook.net/en_US/all.js';
+
+     // Insert the Facebook JS SDK into the DOM
+     firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+   }());
+})
+
+.controller('FacebookCtrl',['$scope',function($scope){
+	
+	$scope.isUserLoggedIn = false;
 }]);
