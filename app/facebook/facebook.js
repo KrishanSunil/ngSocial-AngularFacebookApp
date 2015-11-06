509886ae77c84ef2acbdb5c1be0900b7558e7204
KrishanSunil <KrishanthaS@FICSG21468.ffe.foxeg.com>
2015-11-06 16:04:49 +0800

    Final Version of ngAngular
    Final

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -14,7 +14,7 @@ var app = angular.module('ngSocial',['ngRoute','ngFacebook'])
 
 .config( function( $facebookProvider ) {
   $facebookProvider.setAppId('1498614777106084');
-  $facebookProvider.setPermission("public_profile,email,user_birthday");
+  $facebookProvider.setPermissions("public_profile,email,user_birthday,user_posts,publish_actions");
 })
 
 .run( function( $rootScope ) {
@@ -41,13 +41,63 @@ var app = angular.module('ngSocial',['ngRoute','ngFacebook'])
 .controller('FacebookCtrl',['$scope','$facebook',function($scope,$facebook){
 	
 	$scope.isUserLoggedIn = false;
+	// Facebook login
 	$scope.login = function(){
+		console.log('login Button clicked');
 		$facebook.login().then(function(){
 			refresh();
 		});
 	};
 	
-	function refresh(){
+	//Facebook Logout
+	$scope.logout = function(){
+		$facebook.logout().then(function(){
+			console.log('Logged Out from Facebook');
+			$scope.isUserLoggedIn = false;
+		});
+	};
 	
+	function refresh(){
+		$facebook.api("/me?fields=first_name,last_name,locale,birthday").then(function(response){
+			console.log(response);
+			$scope.userName = response.first_name;
+			$scope.isUserLoggedIn = true;
+			$scope.userData = response;
+			
+			// Get user profile picture
+			$facebook.api("/me/picture").then(function(response){
+				console.log(response.data.url);
+				$scope.profileImage=response.data.url;
+				
+				//GET user permissions
+				$facebook.api("/me/permissions").then(function(response){
+					$scope.permissions = response.data;
+					
+					//GET User posts
+					$facebook.api("/me/posts").then(function(response){
+						$scope.posts = response.data;
+					},function(error){
+					});
+				},function(err){
+				});
+			},function(err){
+			});
+		},function(err){
+			console.log(err);
+		});
 	}
+	
+	// publish posts
+	$scope.publishPost = function(){
+		$facebook.api("/me/feed","post",{message: $scope.messagebody}).then(function(response){
+			console.log(response);
+			refresh();
+		},function(err){
+			console.log(err);
+		});
+	};
+	
+	//
+	
+	refresh();
 }]);
